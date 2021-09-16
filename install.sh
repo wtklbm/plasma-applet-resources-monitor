9c9664c2ed8814270951058c738e0c20646fbb2d
wtklbm <wtklbm@gmail.com>
2021-09-16 21:54:53 +0800

    🔧 build: 添加构建脚本

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo rm -rf /usr/share/plasma/plasmoids/org.kde.resourcesMonitor
+rm -rf ./build
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+make
+sudo make install
+
+rm ~/.cache/plasma*.kcache
+
+plasmapkg2 -u package
+
+killall plasmashell
+kstart5 plasmashell
+
+if command -v latte-dock &>/dev/null; then
+    (bash -c 'killall latte-dock; latte-dock' &)
+fi
